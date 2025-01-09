940a6dff7bafbe46346dba3fb51aeb15f0cc7ce6
dhayak <dhayanithi788@gmail.com>
2025-01-09 17:15:22 +0530

    added new file

--- src/Components/MyWork/Mywork.jsx
+++ src/Components/MyWork/Mywork.jsx
@@ -19,7 +19,7 @@ const Mywork = () => {
             return <img key={index} src={work.w_img} alt="" />
         })} */}
         <img src={food} alt="" href='https://www.facebook.com/' />
-        <img src={weather} alt="" />
+        <img src={weather} alt="" href="https://weatherappbydhaya.netlify.app/"/>
       </div>
       <div className="mywork-showmore">
         <p>Show More</p>
